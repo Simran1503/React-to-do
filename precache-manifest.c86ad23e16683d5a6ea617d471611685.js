1a5ec6bb95226df571d63d8d9418cf39faace124
Simran1503 <simran.budhiraja1503@gmail.com>
2020-07-29 17:31:05 +0530

    Updates

---
+++ precache-manifest.c86ad23e16683d5a6ea617d471611685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197dd88b46f8651354c88dfb1755bf13",
+    "url": "/React-to-do/index.html"
+  },
+  {
+    "revision": "926432939184f7cc1269",
+    "url": "/React-to-do/static/css/main.3e6bd53a.chunk.css"
+  },
+  {
+    "revision": "24104087e465366f424a",
+    "url": "/React-to-do/static/js/2.383b5c07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-to-do/static/js/2.383b5c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "926432939184f7cc1269",
+    "url": "/React-to-do/static/js/main.9c99f9d3.chunk.js"
+  },
+  {
+    "revision": "3a78fa69e2700a2a4077",
+    "url": "/React-to-do/static/js/runtime-main.dd2b8869.js"
+  }
+]);
